72f4fffdd185f76803bfbb37f99b8100033b426e
Ben Gurney <ben@webhappens.co.uk>
2020-02-28 14:15:39 +0000

    Update tests

--- tests/WithCallTest.php
+++ tests/WithCallTest.php
@@ -5,16 +5,8 @@ namespace WebHappens\ConditionalMethods\Tests;
 use BadMethodCallException;
 use WebHappens\ConditionalMethods\Tests\Stubs\Model;
 
-class NormalBehaviourTest extends TestCase
+class WithCallTest extends TestCase
 {
-    /** @test */
-    public function normal_methods_work()
-    {
-        $model = new Model;
-
-        $this->assertEquals('FOO', $model->toUpper('foo'));
-    }
-
     /** @test */
     public function missing_methods_throw_bad_method_call_exception()
     {
